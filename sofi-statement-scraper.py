ed406eb48368e4fd89da2bb3ee751ea3d9f6bd37
Lung Chen <lungheichen@gmail.com>
2024-02-07 11:54:45 -0500

    add feature to extract more than 2 pages, and fix bug of not extracting last row of a page

--- sofi-statement-scraper.py
+++ sofi-statement-scraper.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             os.path.join("statements", file),
             pages="1",
             multiple_tables=False,
-            area=(400, 0, 550, 600),
+            area=(400, 0, 555, 600),
             lattice=True,
             pandas_options={
                 "names": ["DATE", "TYPE", "DESCRIPTION", "AMOUNT", "BALANCE"],
@@ -30,48 +30,56 @@ if __name__ == "__main__":
             },
         )
 
-        df2 = page_one_dfs[0]
-        df2.iloc[:, 0] = pd.to_datetime(
-            df2.iloc[:, 0], format="%b %d, %Y", errors="coerce"
+        df_last = page_one_dfs[0]
+        df_last.iloc[:, 0] = pd.to_datetime(
+            df_last.iloc[:, 0], format="%b %d, %Y", errors="coerce"
         )
 
-        if df2.empty:
+        if df_last.empty:
             continue
-        transformed_dfs.append(df2)
+        transformed_dfs.append(df_last)
 
-        # next_page_dfs =
-        page_two_dfs = tb.read_pdf(
-            os.path.join("statements", file),
-            pages="2",
-            lattice=True,
-            multiple_tables=False,
-            area=(40, 0, 800, 600),
-            columns=[100, 200, 400, 500, 600],
-            pandas_options={
-                "header": None,
-            },
-        )
+        page_has_table = True
+        page = 2
+        while page_has_table:
+            next_page_dfs = tb.read_pdf(
+                os.path.join("statements", file),
+                pages=page,
+                lattice=True,
+                multiple_tables=False,
+                area=(40, 0, 800, 600),
+                columns=[100, 200, 400, 500, 600],
+                pandas_options={
+                    "header": None,
+                },
+            )
 
-        df1 = page_two_dfs[0]
+            if len(next_page_dfs) == 0:
+                break
+            df = next_page_dfs[0]
 
-        df1.iloc[:, 0] = pd.to_datetime(
-            df1.iloc[:, 0], format="%b %d, %Y", errors="coerce"
-        )
+            df.iloc[:, 0] = pd.to_datetime(
+                df.iloc[:, 0], format="%b %d, %Y", errors="coerce"
+            )
 
-        bad_indices = df1[df1[0].isnull()]
-        if len(bad_indices) > 0:
-            first_bad_index = bad_indices.index[0]
-        else:
-            first_bad_index = -1
-        df1 = df1[:first_bad_index].dropna(axis=1)
+            bad_indices = df[df[0].isnull()]
+            if len(bad_indices) > 0:
+                first_bad_index = bad_indices.index[0]
+                df = df[:first_bad_index].dropna(axis=1)
+            else:
+                # No bad indices; all data is good
+                df = df.dropna(axis=1)
 
-        # if len(df1.columns) > 0:
-        if not df1.empty:
-            df1.columns = ["DATE", "TYPE", "DESCRIPTION", "AMOUNT", "BALANCE"]
-            transformed_dfs.append(df1)
+            # if len(df.columns) > 0:
+            if df.empty:
+                page_has_table = False
+            else:
+                df.columns = ["DATE", "TYPE", "DESCRIPTION", "AMOUNT", "BALANCE"]
+                transformed_dfs.append(df)
+                page += 1
 
         # # Read remote pdf into a list of DataFrame
-        # page_two_dfs = tb.read_pdf(
+        # next_page_dfs = tb.read_pdf(
         #     "https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf"
         # )
 
